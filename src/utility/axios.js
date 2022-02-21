49749c24184ea486825e88c69c297b67c8e79605
Leon Chinn <airqin0224@gmail.com>
2022-02-21 22:25:59 +1100

    url setting

--- src/utility/axios.js
+++ src/utility/axios.js
@@ -2,8 +2,7 @@ import _axios from "axios";
 
 const axios = (baseURL) => {
   const instance = _axios.create({
-    baseURL:
-      baseURL || process.env.REACT_APP_API_DOMAIN || "http://localhost:3004",
+    baseURL: baseURL || process.env.REACT_APP_API_DOMAIN,
     timeout: 1000,
   });
 
